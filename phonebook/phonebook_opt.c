9f1e3d2e4c1220e49e2185f06ffa840db4fef10d
vic85821 <vic85821@gmail.com>
2016-09-24 23:29:17 +0800

    modify phonebook.h(structure) to improve

--- phonebook/phonebook_opt.c
+++ phonebook/phonebook_opt.c
@@ -1,15 +1,44 @@
-#include <stdlib.h>
+/*#include <stdlib.h>
 
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastname[], entry *pHead)
-{
-    /* TODO: implement */
+//entry *findName(char lastname[], entry *pHead)
+//{
+    /* TODO: implement 
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
     return NULL;
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "phonebook_opt.h"
+
+/* original version */
+entry *findName(char lastname[], entry *pHead)
+{
+    while (pHead != NULL) {
+        if (strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+    return NULL;
+}
+
+entry *append(char lastName[], entry *e)
+{
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
 }
--- phonebook/phonebook_opt.h
+++ phonebook/phonebook_opt.h
@@ -5,9 +5,12 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+
+#define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+    
+		char lastName[MAX_LAST_NAME_SIZE]; 
+		union{
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +20,7 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+		} data;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
