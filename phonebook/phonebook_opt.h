984d3c7a2101b63070510696b7f69dacda887796
vic85821 <vic85821@gmail.com>
2016-09-25 10:03:48 +0800

    modify structure (v2)

--- phonebook/phonebook_opt.h
+++ phonebook/phonebook_opt.h
@@ -7,22 +7,30 @@
  * line to set OPT properly */
 
 #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    
-		char lastName[MAX_LAST_NAME_SIZE]; 
-		union{
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
-		} data;
-    struct __PHONE_BOOK_ENTRY *pNext;
+typedef struct __PHONE_BOOK_DETAIL {
+	union{
+        char lastName[MAX_LAST_NAME_SIZE]; 
+			  char firstName[16];
+        char email[16];
+        char phone[10];
+        char cell[10];
+        char addr1[16];
+        char addr2[16];
+        char city[16];
+        char state[2];
+        char zip[5];
+		};
+} detail;
+
+typedef struct _PHONE_BOOK_ENTRY {
+	
+    union{
+        char lastName[MAX_LAST_NAME_SIZE]; 
+		    detail* detailDate;
+		};
+		struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
